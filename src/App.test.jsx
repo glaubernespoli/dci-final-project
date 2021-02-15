4988f3a6ae9642aca01e6a318b96442753713519
glauber.nespoli <glauber.nespoli@gmail.com>
2021-02-15 15:26:26 +0100

    app test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  const h1Text = screen.getByText(`This is the beginning of something.`);
+  expect(h1Text).toBeInTheDocument();
 });
