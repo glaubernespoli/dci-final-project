7eb64abcac7f5ffe0b993b93d2cf781a66584170
glauber.nespoli <glauber.nespoli@gmail.com>
2021-02-18 15:39:08 +0100

    eslint

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   render(<App />);
-  const h1Text = screen.getByText(`This is the beginning of something.`);
+  const h1Text = screen.getByText('This is the beginning of something.');
   expect(h1Text).toBeInTheDocument();
 });
