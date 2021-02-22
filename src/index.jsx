93d5d394be8b8f33dee7a4634aaa3c1f97c5a7fc
glauber.nespoli <glauber.nespoli@gmail.com>
2021-02-22 15:03:06 +0100

    dark theme changes

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { CssBaseline, Paper } from '@material-ui/core';
+import { CssBaseline, StyledEngineProvider } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -13,12 +13,12 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Paper elevation={0} square>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
         <App />
-      </Paper>
-    </ThemeProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
