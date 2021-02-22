6d4dc078eecabea91d7f54a4db9a9a7400cef695
Manonmani-s <mano.hicet@gmail.com>
2021-02-22 14:55:11 +0100

    Footer updated App and Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
+import Footer from './Components/FooterComponent/Footer';
 
 const App = () => (
   <>
-    <h1>This is the beginning of something!</h1>
+    <Footer />
   </>
 );
 
