b4876234117fc5e3f15499cea32e0bc37777657f
Manonmani-s <mano.hicet@gmail.com>
2021-02-24 18:55:32 +0100

    About Us update

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -1,4 +1,5 @@
 import { Typography } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 import React from 'react';
 import useStyles from './text';
 
@@ -6,32 +7,64 @@ export default function Aboutus() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <div className={classes.img1}>
-        <div className={classes.txt}>
+    <container className={classes.root}>
+      <Box className={classes.img1}>
+        <Box className={classes.txt}>
           <Typography variant="overline" className={classes.border}>
             -- About Us --.
             <Typography variant="overline" className={classes.quote}>
               Music is the universal language of Mankind
             </Typography>
           </Typography>
-        </div>
-        <section className={(classes.section, classes.sec1)}>
-          <h2> Section</h2>
-          <div className={classes.sample}>
-            <div className={classes.imgContent}>img </div>
-            <div className={classes.content}>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo quas voluptas
-                minima error natus! Eligendi velit quam exercitationem dolorum, id unde hic quas
-                dolore recusandae et accusantium pariatur, aspernatur magnam, quaerat magni
-                explicabo consequatur repudiandae voluptatibus error? Aliquid, eveniet sapiente?
-                Ipsum deserunt dignissimos nesciunt voluptatibus nisi rem, ut nam accusantium.
-              </p>
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
+        </Box>
+      </Box>
+      <Box className={classes.img2}>
+        <Box className={(classes.section, classes.sec1)}>
+          <Typography variant="h4">Our Story</Typography>
+
+          <Box className={classes.sample}>
+            <Box className={classes.imgContent}>--Text-- </Box>
+            <Box className={classes.content}>
+              <Typography variant="p">
+                Where words fail, music speaks. It speaks of the pain, of the sorrow, of the lost,
+                of the life we live. It shares emotions. It is a way to connect, to understand what
+                others feel. Where words fail, music speaks. It tells the truth whether you want it
+                to or not. Music shares the souls of those we are around, of those in the world that
+                we are living.
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      <Box className={classes.img3}>
+        <Box className={(classes.section, classes.sec2)}>
+          <Typography variant="h4">Achievements</Typography>
+
+          <Box className={classes.sample}>
+            <Box className={classes.content}>
+              <Typography variant="p">
+                Music brings joy, to all of our hearts, It is one of those, emotional arts. Sounds
+                of melodies, that we truly adore, Brings us pure pleasure, as our spirits do soar.
+                Music that touches us, we cant help but smile, We are free to choose, genre or
+                style. Music clearly, enlightens our days, Makes us happy, in so many ways.
+              </Typography>
+            </Box>
+            <Box className={classes.imgContent} />
+          </Box>
+        </Box>
+      </Box>
+      <Box className={classes.img4}>
+        <Typography variant="h4" className={classes.section}>
+          Gallery
+        </Typography>
+
+        <Box className={(classes.section, classes.sec3, classes.gallery)}>
+          <Box className={classes.imgGallery}>Awards 2019</Box>
+          <Box className={classes.imgGallery}>Euphoria 2020</Box>
+          <Box className={classes.imgGallery}>Musica 2020</Box>
+          <Box className={classes.imgGallery}>Event 2019</Box>
+        </Box>
+      </Box>
+    </container>
   );
 }
--- src/components/Text/text.js
+++ src/components/Text/text.js
@@ -1,17 +1,11 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Img1 from '../Images/img1.jpg';
+import Img2 from '../Images/img2.jpg';
+import Img3 from '../Images/img3.jpg';
+import Img5 from '../Images/img4.jpg';
+import Img4 from '../Images/img5.jpg';
 
 const useStyles = makeStyles({
-  // root: {
-  //   height: '100%',
-  //   margin: '0',
-  //   fontSize: '16px',
-  //   fontFamily: 'Karla',
-  //   lineHeight: '1.8em',
-  //   fontWeight: '400',
-  //   color: '#666',
-  //   backgroundColor: '#000'
-  // },
   img1: {
     backgroundImage: `url(${Img1})`,
     backgroundPosition: 'center',
@@ -19,21 +13,63 @@ const useStyles = makeStyles({
     backgroundRepeat: 'no-repeat',
     width: '100vw',
     height: '100vh',
-
     backgroundAttachment: 'fixed'
   },
+
+  img2: {
+    backgroundImage: `url(${Img2})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    backgroundAttachment: 'fixed',
+    minHeight: '700px'
+  },
+  img3: {
+    backgroundImage: `url(${Img3})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    backgroundAttachment: 'fixed',
+    minHeight: '800px'
+  },
+  img4: {
+    backgroundImage: `url(${Img1})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    backgroundAttachment: 'fixed',
+    minHeight: '800px'
+  },
   section: {
-    textAlign: 'center',
-    padding: '50px 80px'
+    textAlign: 'center'
   },
   sec1: {
-    /* background-color: #f4f4f4; */
     textAlign: 'center',
     backgroundColor: 'black',
     color: '#666',
-    borderRadius: '100px',
-    background: 'linear-gradient(90deg, #333 20%, #3333 40%, rgb(22, 21, 21) 90%)'
+    borderRadius: '80px',
+    background: 'linear-gradient(90deg, #333 20%, #3333 40%, rgb(22, 21, 21) 90%)',
+    position: 'absolute',
+    bottom: '0'
   },
+  sec2: {
+    color: '#666',
+    borderRadius: '100px',
+    // background: 'linear-gradient(90deg, rgb(22, 21, 21) 10%,#333 30%, #3333 90%)',
+    background: 'linear-gradient(90deg, rgb(22, 21, 21) 20%, #3348 60%, #333 90%)',
+    padding: '10px 30px'
+  },
+  sec3: {
+    backgroundColor: 'black',
+    color: '#666',
+    borderRadius: '100px',
+    background: 'linear-gradient(90deg, #333 10%, #333 50%, #333 90%)',
+    padding: '10px 30px'
+  },
+
   sample: {
     display: 'flex'
   },
@@ -43,12 +79,15 @@ const useStyles = makeStyles({
   },
   imgContent: {
     flex: '50%',
-    backgroundImage: `url(${Img1})`,
-
+    alignSelf: 'flex-start',
+    backgroundImage: `url(${Img4})`,
     backgroundSize: '200px 200px',
     backgroundRepeat: 'no-repeat',
-
-    color: 'red'
+    backgroundPosition: 'center',
+    minHeight: '100px'
+    // '&:hover': {
+    //   opacity: '.7'
+    // }
   },
   txt: {
     position: 'absolute',
@@ -72,6 +111,30 @@ const useStyles = makeStyles({
     fontFamily: 'cursive',
     textTransform: 'capitalize',
     letterSpacing: '3px'
+  },
+  gallery: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '1rem',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  imgGallery: {
+    backgroundImage: `url(${Img5})`,
+    marginTop: '3rem',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+
+    width: '30vw',
+    height: '30vh',
+    textAlign: 'center',
+    fontStyle: 'italic',
+    fontFamily: 'cursive',
+    borderRadius: '20px',
+    '&:hover': {
+      opacity: '.7'
+    }
   }
 });
 export default useStyles;
