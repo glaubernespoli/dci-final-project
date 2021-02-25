908a289db8cb8149c5d714a5d41a34017d0418c9
santiagoejm <sjulian983@gmail.com>
2021-02-25 10:40:39 +0100

    minor footer style fixed

--- src/component/FooterComponent/footerStyle.js
+++ src/component/FooterComponent/footerStyle.js
@@ -1,13 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
-  root: {
-    flexGrow: 1,
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    width: '100%'
-  },
+  // root: {
+  //   flexGrow: 1,
+  //   position: 'absolute',
+  //   bottom: 0,
+  //   left: 0,
+  //   width: '100%'
+  // },
 
   footerStyle: {
     display: 'flex',
