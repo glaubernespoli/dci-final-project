4ff35dd287f13442a23926969411339db454bd0e
santiagoejm <sjulian983@gmail.com>
2021-02-26 11:51:39 +0100

    Product Show List 1.0

---
+++ src/Components/product-list/APIproductShow.js
@@ -0,0 +1,81 @@
+const Products = [
+  {
+    title: 'API Product #1',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #2',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1487180144351-b8472da7d491?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8bXVzaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #3',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1458560871784-56d23406c091?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8bXVzaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #4',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1459749411175-04bf5292ceea?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fG11c2ljfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #5',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #6',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1483000805330-4eaf0a0d82da?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8bXVzaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #7',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1458560871784-56d23406c091?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8bXVzaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #8',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1487180144351-b8472da7d491?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8bXVzaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #9',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1483000805330-4eaf0a0d82da?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8bXVzaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #10',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1459749411175-04bf5292ceea?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fG11c2ljfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title: 'API Product #11',
+    subtitle: 'Detail description of product',
+    author: 'Jon Doe',
+    img:
+      'https://images.unsplash.com/photo-1483000805330-4eaf0a0d82da?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8bXVzaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  }
+];
+
+export default Products;
