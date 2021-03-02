84bead7d2d60933459dc2865765c424b9dedee26
santiagoejm <sjulian983@gmail.com>
2021-03-02 11:24:43 +0100

    info component updated

--- src/component/mainContent/Information/Information.jsx
+++ src/component/mainContent/Information/Information.jsx
@@ -1,4 +1,4 @@
-import { Container, Paper, Typography } from '@material-ui/core';
+import { Grid, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 import InfoStyle from './InfoStyle';
 
@@ -7,11 +7,38 @@ export default function FixedContainer() {
   return (
     <>
       <Paper>
-        <Container fixed className={classes.root}>
-          <Typography variant="h2" color="inherit" align="center" className={classes.info}>
-            We have not decided Yet?
-          </Typography>
-        </Container>
+        <Grid container className={classes.root}>
+          <Grid item xs={12} sm={6}>
+            <img
+              src="https://www.discogs.com/images/app/phones.png"
+              alt="googleplay"
+              className={classes.imagePhone}
+            />
+          </Grid>
+          <Grid
+            container
+            xs={12}
+            sm={6}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Typography variant="h6">
+                Next!
+                <em> Download SYR App </em>
+                Here!
+              </Typography>
+            </Grid>
+            <Grid item className={classes.imageIconContainer}>
+              <img
+                src="https://www.discogs.com/images/app/google-play-badge.png"
+                alt="googleplay icon"
+                className={classes.imageIcon}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
       </Paper>
     </>
   );
