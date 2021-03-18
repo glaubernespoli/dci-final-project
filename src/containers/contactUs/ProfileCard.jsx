4ddb5e9070dc8366a55a211a9b32c370c98004f7
arifur-ra <ararif_017@yahoo.com>
2021-03-18 15:32:13 +0100

    destructure contact us page

---
+++ src/containers/contactUs/ProfileCard.jsx
@@ -0,0 +1,35 @@
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React from 'react';
+import contactUs from './ContactUsStyle';
+
+const ProfileCard = ({ name, designation, img }) => {
+  const classes = contactUs();
+
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia component="img" alt="G" height="220" image={img} className={classes.image} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              <span className={classes.span1}>{name}</span>
+            </Typography>
+            <Typography variant="h6" color="textSecondary">
+              {designation}
+            </Typography>
+            <Typography variant="h6" color="textSecondary">
+              <h6> JavaScript || React || NodeJs || MongoDB || BootStrap || Sass || JQuery ||</h6>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
+};
+ProfileCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  designation: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired
+};
+export default ProfileCard;
