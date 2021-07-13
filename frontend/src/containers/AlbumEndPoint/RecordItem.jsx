bd0fbb741b41c626a4688d0de858290be429d333
Santiago Julian <68547995+santiagoejm@users.noreply.github.com>
2021-07-13 10:59:30 +0200

    profile page (#97)
    * profile page
    
    * euro sign added

--- frontend/src/containers/AlbumEndPoint/RecordItem.jsx
+++ frontend/src/containers/AlbumEndPoint/RecordItem.jsx
@@ -45,27 +45,37 @@ const RecordItem = ({ itemId }) => {
                 {item.name}
               </Typography>
               <Typography variant="h6" component="div">
-                Label:
-                {item.label}
+                Label: &nbsp;
+                <Typography variant="subtitle1" display="inline">
+                  {item.label}
+                </Typography>
               </Typography>
               <Typography variant="h6" component="div">
-                Published Date:
-                {formattedDate}
+                Published Date: &nbsp;
+                <Typography variant="subtitle1" display="inline">
+                  {formattedDate}
+                </Typography>
               </Typography>
               <Typography variant="h6" component="div">
-                Country:
-                {item.country}
+                Country: &nbsp;
+                <Typography variant="subtitle1" display="inline">
+                  {item.country}
+                </Typography>
               </Typography>
               <Typography variant="h6" component="div">
-                Format:
-                {item.format}
+                Format: &nbsp;
+                <Typography variant="subtitle1" display="inline">
+                  {item.format}
+                </Typography>
               </Typography>
               <Typography variant="h6" component="div">
-                Style:
-                {item.style}
+                Style: &nbsp;
+                <Typography variant="subtitle1" display="inline">
+                  {item.style}
+                </Typography>
               </Typography>
               <Typography variant="h6" component="div">
-                {item.price}
+                {`${item.price}€`}
               </Typography>
 
               <Button variant="outlined" endIcon={<AddShoppingCartIcon />}>
